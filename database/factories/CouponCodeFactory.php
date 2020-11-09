f3b48e87aa6191ed1f6e224eaf228dc0b77bd2a4
zhaobin <qq1013446945@126.com>
2020-11-09 16:36:40 +0800

    cs-shop

---
+++ database/factories/CouponCodeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\CouponCode;
+use Faker\Generator as Faker;
+
+$factory->define(CouponCode::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
